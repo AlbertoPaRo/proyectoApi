d93e0787f566e8601126a3645c9abc515c176ab3
AlbertoPaRo <albertopadillaroj@gmail.com>
2022-08-25 23:49:07 -0400

    variable renombrada

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-const { request } = require("express");
 const express = require("express");
 
 const app = express();
-const mimiddleware = (req, res, next) => {
+const miMiddleware = (req, res, next) => {
   if (req.method === "GET") {
     console.log(req.method);
     console.log("hola mundo");
   } else {
-    res.send("metodo no permitido");
+    res.send("method no permitido");
   }
   next();
 };
 
-app.get("/", mimiddleware, function (req, res) {
+app.get("/", miMiddleware, function (req, res) {
   res.send("maik se esta emputando");
 });
 
-app.use(mimiddleware);
+app.use(miMiddleware);
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
